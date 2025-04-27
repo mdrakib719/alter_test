289a0f34e8fc594eb8dd8fcee1bc7f1aac959be4
Maruf Ibnee Mijan <83586928+MarufIbneeMijan@users.noreply.github.com>
2025-04-27 23:35:34 +0600

    changed

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -12,82 +12,36 @@ export default function Team() {
     {
       name: "Sahoria Ahmmad Durjoy",
       role: "Team Lead",
-<<<<<<< HEAD
-=======
-      image:
-        "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/blog-images//Screenshot%202025-04-22%20at%2012.55.42%20AM.png",
-    },
-    {
-      name: "Farha Hassan Priti",
-      role: "Electronics And Communication Lead",
->>>>>>> 580b7f3edf320a93dab7c8faf63e42b0b2e97f27
       image:
         "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.36%20(1).jpeg",
     },
     {
-<<<<<<< HEAD
       name: "Farha Hassan Priti",
       role: "Electronics And Communication Lead",
-=======
-      name: "Mehidi Hassan",
-      role: "Control And AI Lead",
->>>>>>> 580b7f3edf320a93dab7c8faf63e42b0b2e97f27
       image:
         "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.37.jpeg",
     },
     {
-<<<<<<< HEAD
       name: "Mehidi Hassan",
       role: "Control And AI Lead",
-=======
-      name: "Anamul Haque Akash",
-      role: "Management Lead",
->>>>>>> 580b7f3edf320a93dab7c8faf63e42b0b2e97f27
       image:
         "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.36%20(2).jpeg",
     },
     {
-<<<<<<< HEAD
       name: "Anamul Haque Akash",
       role: "Management Lead",
-=======
-      name: "Mahadia Mashkura",
-      role: "Management Co-Lead ",
->>>>>>> 580b7f3edf320a93dab7c8faf63e42b0b2e97f27
       image:
         "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.38.jpeg",
     },
     {
-<<<<<<< HEAD
       name: "Mahadia Mashkura",
       role: "Management Co-Lead",
-=======
-      name: "Raheeb Sadman",
-      role: "Electronics Co-Lead",
->>>>>>> 580b7f3edf320a93dab7c8faf63e42b0b2e97f27
       image:
         "https://i.ibb.co.com/q3fJF1sf/Whats-App-Image-2025-04-21-at-9-53-33-PM.jpg",
     },
     {
-<<<<<<< HEAD
       name: "Raheeb Sadman",
       role: "Electronics Co-Lead",
-=======
-      name: "Jahedul Alam Mahee",
-      role: "Comunicatipn Co-Lead",
-      image:
-        "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.37%20(1).jpeg",
-    },
-    {
-      name: "Safwan Sattar",
-      role: "Faculty Advisor",
-      image:
-        "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.36.jpeg",
-    },
-    {
-      name: "Team Member 8",
-      role: "Faculty Advisor",
->>>>>>> 580b7f3edf320a93dab7c8faf63e42b0b2e97f27
       image:
         "https://czrzkrlkqywcczazeopo.supabase.co/storage/v1/object/public/alter//WhatsApp%20Image%202025-04-23%20at%2019.40.35.jpeg",
     },
